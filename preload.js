bee07a29e1dbb519dc87d6cc8dfeffde4497a503
hutinoatari <hutinoatari@gmail.com>
2021-03-25 01:01:05 +0900

    データの読み書きをフォームに紐付けた

--- preload.js
+++ preload.js
@@ -1,21 +1,5 @@
-const {contextBridge, ipcRenderer} = require("electron");
-const fs = require("fs");
+const {contextBridge} = require("electron");
 
 contextBridge.exposeInMainWorld("requires", {
-    fsRead: (file) => {
-        fs.readFile(file, "utf-8", (err, data) => {
-            if(data){
-                return data;
-            }else{
-                throw err;
-            }
-        });
-    },
-    fsWrite: (file, text) => {
-        fs.writeFile(file, text, "utf-8", (err) => {
-            if(err){
-                throw err;
-            }
-        });
-    }
+    fs: require("fs"),
 });
--- src/main.js
+++ src/main.js
@@ -1,2 +1,67 @@
-const fsRead = window.requires.fsRead;
-const fsWrite = window.requires.fsWrite;
+const fs = window.requires.fs;
+
+const nameListfile = "./src/data/nameList.txt";
+
+const nameForm = document.getElementById("nameForm");
+const temperatureForm = document.getElementById("temperatureForm");
+const nameList = document.getElementById("nameList");
+const resetButton = document.getElementById("resetButton");
+const sendButton = document.getElementById("sendButton");
+
+const dateToYYYYMMDD = (date) => {
+    const y = date.getFullYear();
+    const m = date.getMonth() + 1;
+    const d = date.getDate();
+    return `${y}${(""+m).padStart(2, "0")}${(""+d).padStart(2, "0")}`;
+}
+
+const sendForms = () => {
+    const name = nameForm.value;
+    const temperature = temperatureForm.value;
+    if(name.trim()==="" || temperature===""){
+        alert("不正な入力です。");
+        return;
+    }
+
+    const csvFile = `./src/data/${dateToYYYYMMDD(new Date())}.csv`;
+    let csv = fs.readFileSync(csvFile, "utf-8");
+    csv += `${name},${temperature}\n`;
+    fs.writeFileSync(csvFile, csv, "utf-8");
+
+    const data = fs.readFileSync(nameListfile, "utf-8");
+    let names;
+    if(data !== ""){
+        names = data.split("\n");
+        names.push(name);
+        names.sort();
+        names = names.join("\n");
+    }else{
+        names = name;
+    }
+    fs.writeFileSync(nameListfile, names, "utf-8");
+
+    alert("記録完了！");
+    formInit();
+}
+sendButton.onclick = sendForms;
+
+const formInit = () => {
+    nameForm.value = "";
+
+    temperatureForm.value = "";
+
+    nameList.innerHTML = "";
+    const data = fs.readFileSync(nameListfile, "utf-8");
+    if(data !== ""){
+        const names = data.split("\n");
+        names.forEach((name) => {
+            const option = document.createElement("option");
+            const optionText = document.createTextNode(name);
+            option.appendChild(optionText);
+            nameList.appendChild(option);
+        });
+    }
+}
+resetButton.onclick = formInit;
+
+formInit();
