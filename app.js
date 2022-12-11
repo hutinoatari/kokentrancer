e3cdff053f9e5c7ed352aeaed586cf415f3b0a88
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-12-11 16:24:22 +0900

    サーバを書いた

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 
-app.use("/", express.static("public"));
+const dateToYYYYMMDD = (date) => {
+    const y = date.getFullYear();
+    const m = date.getMonth() + 1;
+    const d = date.getDate();
+    return `${y}${(""+m).padStart(2, "0")}${(""+d).padStart(2, "0")}`;
+}
 
-app.listen(port, () => console.log(`app start on port ${port}.`));
+app.use("/", express.static("public"));
+app.use(express.json());
+
+app.listen(port, () => console.log(`app start.`));
+
+app.post("/add", (req, res) => {
+    const {name, temperature} = req.body;
+    if(!fs.existsSync("data")) fs.mkdirSync("data");
+    const path = `data/${dateToYYYYMMDD(new Date())}.csv`;
+    fs.open(path, "a", (err, fd) => {
+        if(err) throw err;
+        fs.appendFile(fd, `${name},${temperature}\n`, (err) => {
+            if(err) throw err;
+            fs.close(fd, (err) => {
+                if(err) throw err;
+            });
+        });
+    });
+    console.log(`${name} fill out.`);
+    res.status(200).send();
+});
--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 const [nameForm, temperatureForm] = document.querySelectorAll("fieldset input");
 const [submitButton, resetButton] = document.querySelectorAll("fieldset button");
 
+const endpoint = "http://localhost:3000";
+
 submitButton.addEventListener("click", () => {
     const name = nameForm.value.trim();
     const temperature = +temperatureForm.value;
@@ -8,7 +10,15 @@ submitButton.addEventListener("click", () => {
         alert("不正な入力です。");
         return;
     }
-    console.log({name, temperature});
+    const temperatureString = Number.isInteger(temperature) ? `${temperture}.0` : ("" + temperature);
+    const confirmMessage = `${name}: ${temperatureString}℃\n以上でよろしいですか？`;
+    if(!window.confirm(confirmMessage)) return;
+    const url = `${endpoint}/add`;
+    fetch(url, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({name, temperature}),
+    });
     nameForm.value = "";
     temperatureForm.value = "";
 });
